89b06fd0b5e6126a4d132d6cd841c4b3dcbdf4b2
alexandre-chevalier <alexandre.chevalier@laplateforme.io>
2024-11-07 10:38:33 +0100

    ajout fichier placement

---
+++ placement.py
@@ -0,0 +1,40 @@
+table= [
+        ["","","x"],
+        ["","","o"],
+        ["x","","x"]
+        ]
+
+def verifCase(table, row, column, symbol):
+        element = table[row][column]
+        print(element)
+        if element == "":
+                element = symbol
+                print(element)
+                return print(element, "au joueur suivant de jouer")
+        else:
+                while element != ""
+                        return print("veuillez choisir une autre case")
+                        row     = int(input("rentrez la ligne : "))
+                        column  = int(input("rentrez la colonne : "))
+
+       
+
+row     = int(input("rentrez la ligne : "))
+column  = int(input("rentrez la colonne : "))
+symbol  = input("veuillez rentrez le signe :  ")
+
+verifCase(table, row, column, symbol)
+
+
+
+
+
+
+
+"""
+ if tabParam == "":
+                
+
+verifCase(row, column, tab, player1)
+
+"""
