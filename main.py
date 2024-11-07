8c2b835fc75c5e9ae4ebfdaca9a2ccc4931ae175
Antoine LECA <antoine.leca@laplateforme.io>
2024-11-07 09:53:59 +0100

    ajout du fichier main qui appelera les fonctions des autres fichiers

---
+++ main.py
@@ -0,0 +1,7 @@
+from joueurs.py import player1
+from joueurs.py import player2
+from grid.py import create_grid
+from placement.py import verifCase
+from winner.py import horizontal
+from winner.py import verticale
+from winner.py import diagonale
