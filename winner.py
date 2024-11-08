c240d97d476791ebb880b428a75e94d16b30295e
emilie-ponce <emilie.ponce@laplateforme.io>
2024-11-08 09:20:56 +0100

    Add files via upload

--- winner.py
+++ winner.py
@@ -1,9 +1,11 @@
+"""
 #plateau de jeu
 player = [
-    ['X', 'O', 'X'],
     ['X', 'X', 'X'],
+    ['X', 'O', 'X'],
     ['O', 'X', 'O']
 ]
+"""
 
 #verification des cases horizontales
 def horizontal(board):
@@ -17,6 +19,9 @@ player[1][0] == "O" and player[1][1] == "O" and player[1][2] == "O":
     elif player[2][0] == "X" and player[2][1] == "X" and player[2][2] == "X" or \
 player[2][0] == "O" and player[2][1] == "O" and player[2][2] == "O":
         return print("victoire2")
+    
+    else:
+        return False
 
 horizontal(player)
 
@@ -34,6 +39,9 @@ player[0][1] == "O" and player[1][1] == "O" and player[2][1] == "O":
     elif player[0][2] == "X" and player[1][2] == "X" and player[2][2] == "X" or \
 player[0][2] == "O" and player[1][2] == "O" and player[2][2] == "O":
         return print("victoire5")
+    
+    else:
+        return False
 
 verticale(player)
 
@@ -48,4 +56,18 @@ player[0][0] == "O" and player[1][1] == "O" and player[2][2] == "O":
 player[0][2] == "O" and player[1][1] == "O" and player[2][0] == "O":
         return print("victoire7")
 
+    else:
+        return False
+
 diagonale(player)
+
+
+#verification match nul
+def match_nul(horizontal, verticale, diagonale):
+    if horizontal(player) == False and verticale(player) == False and \
+diagonale(player) == False:
+        return print("match nul")
+    else:
+        print("nul")
+
+match_nul(horizontal, verticale, diagonale)
