48083f595761974dd1892714788a2236b49018ba
balqisse-messaya <balqisse.messaya@laplateforme.io>
2024-11-08 14:03:27 +0100

    Update joueurs.py

--- joueurs.py
+++ joueurs.py
@@ -10,13 +10,13 @@ def initialiser_joueurs() :
 
 
 
-def initialiser_joueurs() :
+def choisir_symboles() :
     while True:
-        player1 = input("Player1, choisissez votre symbole (X ou O) : ").upper()
+        player1 = input("player1, choisissez votre symbole (X ou O) : ").upper()
         if player1 == "X" or player1 == "O":
             break
         else:
-            print("Valeur incorrecte. Veuillez entrez X ou O.")
+            print("valeur incorrecte. Veuillez entrez X ou O")
     player2 = "O" if player1 == "X" else "X"
     print(f"player1 est {player1}.")
     print(f"player2 est {player2}.")
